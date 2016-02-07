e45fcd8ec5017f683e0449b75a03fdec50aaeccf
William Verdolini <william.verdolini@gmail.com>
2016-02-07 16:04:32 +0100

    complete Repository tests

--- ConfirmationReport.Test/ConfirmationReportRepositoryTests.cs
+++ ConfirmationReport.Test/ConfirmationReportRepositoryTests.cs
@@ -115,5 +115,272 @@ namespace ConfirmRep.Test
             // Assert
             Assert.IsNull(actual);
         }
+
+        [Test]
+        public void FindAllByOwner__If_Reports_Context_is_empty__Return_null()
+        {
+            // Arrange
+            var data = new List<ConfirmationReport> {};
+
+            // Create a DbSet substitute.
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+            context.Reports.AsNoTracking().Returns(set);
+            ReportStatus? status = null;
+
+            // Act
+            var actual = repo.FindAllByOwner("wilver", status);
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.That(actual.Count(), Is.EqualTo(0));            
+        }
+
+        [Test]
+        public void FindAllByOwner__If_Reports_not_contains_owner_items__Return_null()
+        {
+            // Arrange
+            var data = new List<ConfirmationReport> { new ConfirmationReport { Id = 1, OwnerName = "matraf" }, new ConfirmationReport { Id = 2, OwnerName = "giofae" } };
+
+            // Create a DbSet substitute.
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+            context.Reports.AsNoTracking().Returns(set);
+            ReportStatus? status = null;
+
+            // Act
+            var actual = repo.FindAllByOwner("wilver", status);
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.That(actual.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FindAllByOwner__If_Reports_contains_owner_items__Return_item_list()
+        {
+            // Arrange
+            var data = new List<ConfirmationReport> { new ConfirmationReport { Id = 1, OwnerName = "matraf" }, new ConfirmationReport { Id = 2, OwnerName = "giofae" } };
+
+            // Create a DbSet substitute.
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+            context.Reports.AsNoTracking().Returns(set);
+            ReportStatus? status = null;
+
+            // Act
+            var actual = repo.FindAllByOwner("matraf", status);
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.That(actual.Count(), Is.GreaterThan(0));
+        }
+
+        [TestCase("wilver", null, ExpectedResult = 0)]
+        [TestCase("matraf", null, ExpectedResult = 3)]
+        [TestCase("giofae", null, ExpectedResult = 1)]
+        [TestCase("matraf", ReportStatus.Draft, ExpectedResult = 2)]
+        [TestCase("matraf", ReportStatus.Completed, ExpectedResult = 1)]
+        [TestCase("giofae", ReportStatus.Draft, ExpectedResult = 1)]
+        [TestCase("giofae", ReportStatus.Completed, ExpectedResult = 0)]
+        public int FindAllByOwner__If_Reports_contains_owner_items__Return_item_list(string owner, ReportStatus? status)
+        {
+            // Arrange
+            var data = new List<ConfirmationReport> { 
+                new ConfirmationReport { Id = 1, OwnerName = "matraf", Status=ReportStatus.Draft }, 
+                new ConfirmationReport { Id = 2, OwnerName = "matraf", Status=ReportStatus.Draft }, 
+                new ConfirmationReport { Id = 3, OwnerName = "matraf", Status=ReportStatus.Completed }, 
+                new ConfirmationReport { Id = 4, OwnerName = "giofae", Status=ReportStatus.Draft } };
+
+            // Create a DbSet substitute.
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+            context.Reports.AsNoTracking().Returns(set);
+
+            // Act
+            var actual = repo.FindAllByOwner(owner, status);
+
+            // Assert
+            return actual.Count();
+        }
+
+        [Test]
+        public async Task FindNewReportNumber_If_Reports_Context_is_empty__Return_1()
+        {
+            // Arrange
+            //var data = new List<ConfirmationReport> { new ConfirmationReport { Id = 1, OwnerName = "matraf" }, new ConfirmationReport { Id = 2, OwnerName = "giofae" } };
+            var data = new List<ConfirmationReport> {};
+
+            // Create a DbSet substitute.
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+            context.Reports.AsNoTracking().Returns(set);
+
+            // Act
+            var actual = await repo.FindNewReportNumber();
+
+            // Assert
+            //Assert.IsNotNull(actual);
+            Assert.That(actual, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task FindNewReportNumber_If_Reports_Context_is_not_empty__Return_max_ReportNumber_plus_1()
+        {
+            // Arrange
+            var data = new List<ConfirmationReport> { new ConfirmationReport { Id = 1, OwnerName = "matraf", ReportNumber=3 }, new ConfirmationReport { Id = 2, OwnerName = "giofae", ReportNumber=5 } };
+
+            // Create a DbSet substitute.
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+            context.Reports.AsNoTracking().Returns(set);
+
+            // Act
+            var actual = await repo.FindNewReportNumber();
+
+            // Assert
+            //Assert.IsNotNull(actual);
+            Assert.That(actual, Is.EqualTo(6));
+        }
+
+        [Test]
+        public async Task SaveDraft__Report_with_Id0__Is_Added_to_repo()
+        {
+            // Arrange
+            var savedReport = new ConfirmationReport {Id = 0 };
+            // Create a DbSet substitute.
+            var data = new List<ConfirmationReport> { };
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+
+            context.Reports.Add(savedReport).Returns(savedReport);
+            context.SaveChangesAsync().Returns(0);
+
+            // Act
+            await repo.SaveDraft(savedReport);
+
+            // Assert
+            context.Reports.Received().Add(savedReport);
+            await context.Received().SaveChangesAsync();
+            //Assert.That(savedReport.Status, Is.EqualTo(ReportStatus.Draft));
+        }
+
+        [Test]
+        public async Task SaveDraft__Report_with_Id_not0__Is_Updated_into_repo()
+        {
+            // Arrange
+            var savedReport = new ConfirmationReport { Id = 5 };
+            // Create a DbSet substitute.
+            var data = new List<ConfirmationReport> { new ConfirmationReport {Id=5}};
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+            context.Reports.AsNoTracking().Returns(set);
+            context.Reports.Attach(savedReport).Returns(savedReport);
+            context.SaveChangesAsync().Returns(0);
+
+            // Act
+            await repo.SaveDraft(savedReport);
+
+            // Assert
+            context.Reports.Received().Attach(savedReport);
+            await context.Received().SaveChangesAsync();
+            //Assert.That(savedReport.Status, Is.EqualTo(ReportStatus.Draft));
+        }
+
+        [Test]
+        public async Task SaveDraft__Report__Is_Marked_as_Draft()
+        {
+            // Arrange
+            var savedReport = new ConfirmationReport{};
+            // Create a DbSet substitute.
+            var data = new List<ConfirmationReport> { };
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+
+            context.Reports.Add(savedReport).Returns(savedReport);
+            context.SaveChangesAsync().Returns(0);
+
+            // Act
+            await repo.SaveDraft(savedReport);
+
+            // Assert
+            Assert.That(savedReport.Status, Is.EqualTo(ReportStatus.Draft));
+        }
+
+        [Test]
+        public async Task Save__Report_with_Id0__Is_Added_to_repo()
+        {
+            // Arrange
+            var savedReport = new ConfirmationReport { Id = 0 };
+            // Create a DbSet substitute.
+            var data = new List<ConfirmationReport> { };
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+
+            context.Reports.Add(savedReport).Returns(savedReport);
+            context.SaveChangesAsync().Returns(0);
+
+            // Act
+            await repo.Save(savedReport);
+
+            // Assert
+            context.Reports.Received().Add(savedReport);
+            await context.Received().SaveChangesAsync();
+            //Assert.That(savedReport.Status, Is.EqualTo(ReportStatus.Draft));
+        }
+
+        [Test]
+        public async Task Save__Report_with_Id_not0__Is_Updated_into_repo()
+        {
+            // Arrange
+            var savedReport = new ConfirmationReport { Id = 5 };
+            // Create a DbSet substitute.
+            var data = new List<ConfirmationReport> { new ConfirmationReport { Id = 5 } };
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+            context.Reports.AsNoTracking().Returns(set);
+            context.Reports.Attach(savedReport).Returns(savedReport);
+            context.SaveChangesAsync().Returns(0);
+
+            // Act
+            await repo.Save(savedReport);
+
+            // Assert
+            context.Reports.Received().Attach(savedReport);
+            await context.Received().SaveChangesAsync();
+            //Assert.That(savedReport.Status, Is.EqualTo(ReportStatus.Draft));
+        }
+
+        [Test]
+        public async Task Save__Report__Is_Marked_as_Completed()
+        {
+            // Arrange
+            var savedReport = new ConfirmationReport { };
+            // Create a DbSet substitute.
+            var data = new List<ConfirmationReport> { };
+            var set = Substitute.For<DbSet<ConfirmationReport>, IQueryable<ConfirmationReport>, IDbAsyncEnumerable<ConfirmationReport>>()
+                                .SetupData(data);
+            context.Reports.Returns(set);
+
+            context.Reports.Add(savedReport).Returns(savedReport);
+            context.SaveChangesAsync().Returns(0);
+
+            // Act
+            await repo.Save(savedReport);
+
+            // Assert
+            Assert.That(savedReport.Status, Is.EqualTo(ReportStatus.Completed));
+        }
     }
 }
